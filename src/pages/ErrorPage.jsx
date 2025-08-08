858b3d5d343ec5a4f8591ad7ece3ca7e973ced83
MMunim <mmunim9.9.01@gmail.com>
2025-08-08 23:05:26 +0600

    fix and update error page issue

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { FaArrowLeftLong } from "react-icons/fa6";
-import { Link } from "react-router";
+import { Link, useRouteError } from "react-router";
 
 const ErrorPage = () => {
+  const error = useRouteError();
   return (
-    <div className="w-11/12 mx-auto flex flex-col justify-center items-center bg-white pb-20">
-    <img src="https://i.ibb.co/v4THdMnz/404.gif" alt="" />
-    <button className="btn font-bold text-xl py-6 text-center">
+    <div className="flex flex-col justify-center items-center bg-white p-16 min-h-screen">
+      <img src="https://i.ibb.co/v4THdMnz/404.gif" alt="" />
+      <h1 className="text-red-600 font-bold text-4xl mb-4">{error?.status}</h1>
+      <p className="mb-5 text-xl font-bold text-red-600 md:text-2xl">
+        {error?.error?.message || "Something Went Wrong!"}
+      </p>
+      <button className="btn font-bold text-xl py-6 text-center mt-6">
         <FaArrowLeftLong className="inline" /> &nbsp;
         <Link to="/">Back to home</Link>
       </button>
